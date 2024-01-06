15e0bc2042ca307a6be501a92ead5b58825bad5c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-01-06 17:44:34 +0300

    New Commit

--- src/test/java/baseurl/DummyRestApi.java
+++ src/test/java/baseurl/DummyRestApi.java
@@ -1,4 +1,13 @@
 package baseurl;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
 public class DummyRestApi {
+    protected RequestSpecification specification;
+    @Before
+    public void setUpBaseUrl(){
+        specification= new RequestSpecBuilder().setBaseUri("https://dummy.restapiexmaple.com/api/v2/employees").build();
+    }
 }
